c9d8cbc9af5e0d4e1a667d0185ac80943ab72f49
Jabulani Gwenzi <ignertic@icloud.com>
2019-10-29 22:46:04 +0200

    test

--- test/paynow_test.dart
+++ test/paynow_test.dart
@@ -6,8 +6,8 @@ void main() {
   test('check if cart is working', () {
     final paynow = Paynow();
     final payment = paynow.createPayment("reference", "authEmail");
-    payment.add("title", 1.0);
+    payment.add("product", 1.0);
     expect(payment.total(), 1.0);
-    
+
   });
 }
