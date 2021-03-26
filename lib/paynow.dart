5222cf88c28efc477b673931886300cebf8fc8d0
SuperCode Gnertic <ignertic@icloud.com>
2021-03-26 12:02:18 +0200

    Refactor Variable Names

--- lib/paynow.dart
+++ lib/paynow.dart
@@ -18,38 +18,38 @@ class ValueError implements Exception {
 
 /// stream payment status rather than waiting for delay
 class _PaymentStatusStreamManager {
-  final Paynow pObj;
-  final String pollUrl;
+  final Paynow _paynowObject;
+  final String _pollUrl;
 
   Timer _timer;
 
-  final StreamController<StatusResponse> _controller =
+  final StreamController<StatusResponse> _statusTransactionController =
       StreamController<StatusResponse>();
 
-  Stream<StatusResponse> get statusTransactionStream => _controller.stream;
+  Stream<StatusResponse> get statusTransactionStream => _statusTransactionController.stream;
 
-  void _startStream(int duration) {
+  void _startStream(int streamInterval) {
     _timer = Timer.periodic(
-      Duration(seconds: duration),
+      Duration(seconds: streamInterval),
       (timer) async {
         // poll transaction poll-url
-        var _status = await pObj.checkTransactionStatus(pollUrl);
+        var _status = await _paynowObject.checkTransactionStatus(_pollUrl);
 
         // update stream with latest poll url result
-        _controller.sink.add(_status);
+        _statusTransactionController.sink.add(_status);
       },
     );
   }
 
-  _PaymentStatusStreamManager(this.pObj, this.pollUrl, [int interval = 20]) {
-    _startStream(interval);
+  _PaymentStatusStreamManager(this._paynowObject, this._pollUrl, {int streamInterval = 20}) {
+    _startStream(streamInterval);
     //statusTransactionStream.listen((statusResponse) => statusResponse);
   }
 
   /// close timer and stream controller
-  void close() {
+  void dispose() {
     _timer?.cancel();
-    _controller?.close();
+    _statusTransactionController?.close();
   }
 }
 
@@ -337,7 +337,7 @@ class Paynow {
     int streamInterval = 20,
   }) {
     _statusStreamManager =
-        _PaymentStatusStreamManager(this, pollUrl, streamInterval);
+        _PaymentStatusStreamManager(this, pollUrl, streamInterval: streamInterval);
 
     return _statusStreamManager.statusTransactionStream;
   }
@@ -345,7 +345,7 @@ class Paynow {
   /// close [streamTransactionStatus] stream
   void closeStream() {
     if (_statusStreamManager != null) {
-      _statusStreamManager.close();
+      _statusStreamManager.dispose();
     }
   }
 
